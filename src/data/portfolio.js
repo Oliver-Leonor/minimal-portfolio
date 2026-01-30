4b7d2955535b09b0cf5278131c0903549aa7e5b4
Oliver <oliver.leonor90@gmail.com>
2026-01-31 07:42:56 +0800

    Refactor code structure for improved readability and maintainability

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
   {
-    title: 'Memory Game',
-    imgUrl: '/assets/project1.gif',
-    stack: ['HTML', 'CSS', 'JavasScript', 'React'],
-    link: 'https://memory-game-e4ccb5tnf-oliver-leonor.vercel.app/',
+    title: "Memory Game",
+    imgUrl: "/assets/project1.gif",
+    stack: ["HTML", "CSS", "JavasScript", "React"],
+    link: "https://memory-game-e4ccb5tnf-oliver-leonor.vercel.app/",
   },
   {
-    title: 'Puerto Princesa Guide',
-    imgUrl: '/assets/project6.png',
-    stack: ['TypeScript', 'shadcn/ui', 'Google Maps API', 'NextJS',],
-    link: 'https://palawan-guide.vercel.app/',
+    title: "Puerto Princesa Guide",
+    imgUrl: "/assets/project6.png",
+    stack: ["TypeScript", "shadcn/ui", "Google Maps API", "NextJS"],
+    link: "https://palawan-guide.vercel.app/",
   },
   {
-    title: 'E-Wallet API',
-    imgUrl: '/assets/project3.jpg',
-    stack: ['TypeScript', 'API', 'CloudFlare'],
-    link: 'https://wallet-ledger-api.oliver-leonor.workers.dev/ui',
+    title: "E-Wallet API",
+    imgUrl: "/assets/Project3.jpg",
+    stack: ["TypeScript", "API", "CloudFlare"],
+    link: "https://wallet-ledger-api.oliver-leonor.workers.dev/ui",
   },
-]
+];
